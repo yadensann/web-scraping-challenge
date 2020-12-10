ca0251e6eee5fa503086800ff329b6f0725d30cc
oyasumi <yadensann@gmail.com>
2020-12-09 21:24:28 -0700

    fixed errors and got scraping to work

--- mission_to_mars/scrape_mars.py
+++ mission_to_mars/scrape_mars.py
@@ -41,31 +41,37 @@ def scrape():
     article = news_soup.find('div', class_= 'list_text')
     news_title = article.find('div', class_= 'content_title').text
     news_info = article.find('div', class_= 'article_teaser_body').text
-    return news_title, news_info
+#     return news_title, news_info
 
     
     # Featured Image of Mars
 # def feat_image():
-    image_url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+    image_url = "https://www.jpl.nasa.gov/spaceimages/images/wallpaper/PIA24247-640x350.jpg"
     browser.visit(image_url)
     html = browser.html
     image_soup = bs(image_html, 'html.parser')
     image = image_soup.find("img", class_= 'thumb')['src']
     featured_image = "https://www.jpl.nasa.gov" + image
-    return featured_image
+#     return featured_image
     
     
     # Mars Facts
 # def mars_facts():
+
     url = 'https://space-facts.com/mars/'
-    data = pd.read_html(url)
     browser.visit(url)
-    facts_html = browser.html
-    mars_data = pd.DataFrame(data[0])    
-    facts_df.columns = ["Description", "Value"]
-    facts_df = mars_data.set_index("Description")
-    facts = mars_data.to_html(header= False, index= False)    
-    return facts
+    data = pd.read_html(url)
+    mars_df = pd.DataFrame(data[0])
+#     facts = mars_df.to_html(header= False, index= False)
+#     url = 'https://space-facts.com/mars/'
+#     data = pd.read_html(url)
+#     browser.visit(data)
+#     facts_html = browser.html
+#     mars_data = pd.DataFrame(data[0])    
+    mars_df.columns = ["Description", "Value"]
+    clean_df = mars_df.set_index("Description")
+    facts = clean_df.to_html(header= False, index= False)    
+#     return facts
 
     # Mars Hemispheres
 # def mars_hem():
@@ -94,21 +100,22 @@ def scrape():
         hem_dict = {'Title': new_title, 'Image URL': image_link}
         mars_hemispheres.append(hem_dict)
 
-    return mars_hemispheres
+#     return mars_hemispheres
 
 # Dictionary values:
+
     mars_data['news_url'] = news_url
     mars_data['news_title'] = news_title
     mars_data['news_info'] = news_info
-    mars_data['image'] = image
     mars_data['featured_image'] = featured_image
-    mars_data['fact_table'] = facts_df
+    mars_data['facts'] = facts
     mars_data['hemisphere_images'] = mars_hemispheres
     
+    return mars_data
 
 
     browser.quit()
-    return mars_data
+
     
 # def scrape_all():
 #     executable_path = {"executable_path": "/Users/yaden/Downloads/chromedriver"}
