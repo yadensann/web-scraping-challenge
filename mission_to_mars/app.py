cc85111e705756d23d0894742ccc5064c073af6f
oyasumi <yadensann@gmail.com>
2020-12-10 23:47:22 -0700

    got hemisphere scraping to work

--- mission_to_mars/app.py
+++ mission_to_mars/app.py
@@ -4,34 +4,20 @@ import scrape_mars
 
 app = Flask(__name__)
 
-
-
-# Use flask_pymongo to set up mongo connection
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/mars"
-# mongo = PyMongo(app)
-
 client = pymongo.MongoClient('mongodb://localhost:27017')
 db = client.mars
 collection = db.mars
 
-
 @app.route("/")
 def index():
     mars = collection.find_one({})
-#     mars_info = mongo.db.mars.find_one()
     return render_template("index.html", mars = mars)
 
 @app.route("/scrape")
 def web_scrape():
-#     db.collection.remove({})
     mars_data = scrape_mars.scrape()
-#     db.collection.insert_one(mars_data).inserted_id
-#     mongo.db.collection.insert_one(mars_data)
-    
     collection.update({}, mars_data, upsert=True)
-#     mars.update({}, mars_data, upsert=True)
     return redirect("/")
-#     return redirect("http://localhost:5000/", code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- mission_to_mars/scrape_mars.py
+++ mission_to_mars/scrape_mars.py
@@ -15,144 +15,83 @@ def init_browser():
     executable_path = {"executable_path": "/Users/yaden/Downloads/chromedriver"}
     return Browser("chrome", **executable_path, headless=False)
 
-# def scrape():
-#     final_data = {}
-#     output = marsNews()
-#     final_data["Title"] = output[0]
-#     final_data["News"] = output[1]
-#     final_data["Featured Image"] = mars_img()
-#     final_data["Facts About Mars"] = mars_facts()
-#     final_data["Mars' Hemispheres"] = mars_hem()
-    
 def scrape():
     browser = init_browser()
     collection.drop()
     
     mars_data= {}
-    
-    
-    
-# def mars_news():
+
+
     #Latest News About Mars
     news_url = 'https://mars.nasa.gov/news/'
     browser.visit(news_url)
-
     time.sleep(1)
-
     news_html = browser.html
     news_soup = bs(news_html, 'lxml')
     article = news_soup.find('div', class_= 'list_text')
     news_title = article.find('div', class_= 'content_title').text
     news_info = article.find('div', class_= 'article_teaser_body').text
-#     return news_title, news_info
 
-    
     # Featured Image of Mars
-# def feat_image():
     image_url = "https://www.jpl.nasa.gov/spaceimages/images/wallpaper/PIA24247-640x350.jpg"
     browser.visit(image_url)
-
-
     time.sleep(1)
-
     html = browser.html
     image_soup = bs(html, 'html.parser')
     image = image_soup.find("img")['src']
     featured_image=image
-    # featured_image = "https://www.jpl.nasa.gov" + image
-#     return featured_image
-    
-    
-    # Mars Facts
-# def mars_facts():
 
+    # Mars Facts
     url = 'https://space-facts.com/mars/'
     browser.visit(url)
-
-
     time.sleep(1)
-
     data = pd.read_html(url)
     mars_df = pd.DataFrame(data[0])
-#     facts = mars_df.to_html(header= False, index= False)
-#     url = 'https://space-facts.com/mars/'
-#     data = pd.read_html(url)
-#     browser.visit(data)
-#     facts_html = browser.html
-#     mars_data = pd.DataFrame(data[0])    
     mars_df.columns = ["Description", "Value"]
     clean_df = mars_df.set_index("Description")
     facts = clean_df.to_html(header= False, index= False)    
-#     return facts
 
     # Mars Hemispheres
-# def mars_hem():
     hemispheres_url ="https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
     browser.visit(hemispheres_url)
-    
     time.sleep(1)
 
-    hem_html = browser.html
-    hemisphere_soup = bs(html, "html.parser")
-    mars_hemispheres= []
-    # hem_info = hemisphere_soup.find('div', class_= "item")
-    hemispheres = hemisphere_soup.find_all('div', class_ = 'item')
+    first = browser.find_by_tag('h3')[0].text
+    second = browser.find_by_tag('h3')[1].text
+    third = browser.find_by_tag('h3')[2].text
+    fourth = browser.find_by_tag('h3')[3].text
 
-    for hemisphere in hemispheres:
-        browser.visit(hemispheres_url)
-        hem_html = browser.html
-        hem_soup= bs(hem_html, "html.parser")
+    browser.find_by_css('.thumb')[0].click()
+    first_img = browser.find_by_text('Sample')['href']
+    browser.back()
 
-        hem_titles = hem_soup.find('h3').text
-        new_title = hem_titles.replace('Enhanced', '')
+    browser.find_by_css('.thumb')[1].click()
+    second_img = browser.find_by_text('Sample')['href']
+    browser.back()
 
-        ref_link = hem_soup.find("a")["href"]
-        image_link = "https://astrogeology.usgs.gov/" + ref_link            
+    browser.find_by_css('.thumb')[2].click()
+    third_img = browser.find_by_text('Sample')['href']
+    browser.back()
 
-        downloads = hem_soup.find("div", class_="downloads")
-        image_downloads = downloads.find("a")["href"]
+    browser.find_by_css('.thumb')[3].click()
+    fourth_img = browser.find_by_text('Sample')['href']
 
-        hem_dict = {'Title': new_title, 'Image URL': image_link}
-        mars_hemispheres.append(hem_dict)
-
-#     return mars_hemispheres
+    hemisphere_image_urls = [
+    {'title': first, 'img_url': first_img},
+    {'title': second, 'img_url': second_img},
+    {'title': third,  'img_url': third_img},
+    {'title': fourth, 'img_url': fourth_img}
+    ]
 
 # Dictionary values:
-
     mars_data['news_url'] = news_url
     mars_data['news_title'] = news_title
     mars_data['news_info'] = news_info
     mars_data['featured_image'] = featured_image
     mars_data['facts'] = facts
-    mars_data['hemisphere_images'] = mars_hemispheres
+    mars_data['hemisphere_images'] = hemisphere_image_urls
     
     browser.quit()
 
     return mars_data
 
-
-
-
-    
-# def scrape_all():
-#     executable_path = {"executable_path": "/Users/yaden/Downloads/chromedriver"}
-#     browser = Browser("chrome", **executable_path, headless=False)
-#     news_title = mars_news(title)
-#     mars_info = mars_news(info)
-#     image = featured_image(browser)
-#     facts = mars_facts(browser)
-#     hemisphere_images = mars_hem(browser)
-
-#     data = {
-#         "title": title,
-#         "info": info,
-#         "image": featured_image,
-#         "facts": facts,
-#         "hemispheres": mars_hemispheres
-#     }
-
-#     return data 
-
-
-
-    
